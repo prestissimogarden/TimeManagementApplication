716d0761f26756b2e43a6082fc4b00732672435f
presti <alanzenezi@gmail.com>
2024-04-17 21:39:13 -0300

    TMA-2 Misericordia bixo como assim nao da pra ver o meu código??? #comment Tarefa do serviço de tarefas concluída

---
+++ TimeManagementApplication/TaskService/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManagementApplication.TaskService
+{
+    internal class Task
+    {
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public Task(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
